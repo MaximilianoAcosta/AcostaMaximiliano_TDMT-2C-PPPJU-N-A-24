85f7f6fa5dfbabe0813cb29aa68de0deb021d71c
DESKTOP-EVQ3875\Maxi <maxiacosta72@gmail.com>
2024-03-25 21:20:32 -0300

    movement fix

--- Assets/Scripts/PlayerMovementnt.cs
+++ Assets/Scripts/PlayerMovementnt.cs
@@ -8,14 +8,22 @@ public class PlayerMovementnt : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private EntityMovement characterMover;
     private Vector2 HorizontalInput;
-    public void getHorizontalInput(InputAction.CallbackContext context)
+    public void getHorizontalInput(InputAction.CallbackContext ctx)
     {   
-        HorizontalInput = context.ReadValue<Vector2>();
+        HorizontalInput = ctx.ReadValue<Vector2>();
         MovePlayer(HorizontalInput);
     }
+    public void moveOnturn(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            MovePlayer(HorizontalInput);
+        }
+    }
+
     private void MovePlayer(Vector2 direction)
     {
-        Vector3 HorizontalMovement = (transform.right * direction.x + transform.forward*direction.y) * speed * Time.fixedDeltaTime;
+        Vector3 HorizontalMovement = (transform.right * direction.x + transform.forward * direction.y) * speed * Time.fixedDeltaTime;
         characterMover.SetDirection(HorizontalMovement);
     }
 
